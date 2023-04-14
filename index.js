55d27da28efda42a11d89026153b89a8f6e03122
johnsonmach6@gmail.com <johnsonmach6@gmail.com>
2023-04-14 17:11:58 +0100

    .

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 //require("./HTTPParser");
 
 
- const express = require("express"),
+  express = require("express"),
   app = express(),
   // fetch = require("node-fetch"),
- // url = require("url"),
- // http = require("http"),
-  //https = require("https"),
+  url = require("url"),
+  http = require("http"),
+  https = require("https"),
   server = app.listen(process.env.PORT || 12345, () => {
   /*   setTimeout (()=>{
      	console.log(8)
@@ -21,10 +21,10 @@
   _end="XRequest.res[$][1].remove();XRequest.resId--;";
 
 app.use((req, res, next) => {
-  return res.send("hello uu");
+  return res.send("1hello uu");
 	try{
 	var md="./x"+req.path.toUpperCase().replace(/\\$|\/$|$/,"/index.js")
-	vrequire(md)(req,res,next)
+	require(md)(req,res,next)
   }catch(e){
   	next()
   	}

efc80c88bf346d71d63a342bb7b6f089e7142aec
Owens94819 <johnsonmach6@gmail.com>
2023-04-14 12:00:12 +0100

    ...

---
+++ XRequest/index.js
@@ -0,0 +1,42 @@
+
+
+ var _end="XRequest.res[$][1].remove();XRequest.resId--;"
+
+module.exports=(req, res) => {
+  res.setHeader("content-type", "text/javascript");
+  var src= req.query.src;
+  var id = req.query.id;
+  var _http;
+  var end=_end.replace("$",id)
+  
+  try{
+  	
+  src=url.parse(src)
+  
+  "https:"===src.protocol && (_http = https)
+  ||
+  "http:"===src.protocol && (_http = http);
+  
+ // src.headers=req.headers;
+  //console.log(src)
+  //res.write(";;")
+  _http
+    .get(src, (req) => {
+    	 //console.log("ready");
+      req.setEncoding("utf8");
+      res.write(end+`XRequest.res[${id}][0](delete XRequest.res[${id}] &&\``);
+      req.on("data", (ch) => res.write(ch.replace(/[\`\\\$\{]/g, "\\$&")));
+      req.on("end", () => res.end(`\`);`));
+    })
+    .on("error", (e) => {
+      console.log("err", src, e);
+      res.end(end+`XRequest.res[${id}][0]("",delete XRequest.res[${id}])`);
+    });
+
+  }catch(e){
+  	console.error("catch",e)
+      res.end(end+`XRequest.res[${id}][0]("",delete XRequest.res[${id}])`);
+  	return
+   }
+  
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+require("./HTTPParser");
+
+
+ express = require("express"),
+  app = express(),
+   fetch = require("node-fetch"),
+  url = require("url"),
+  http = require("http"),
+  https = require("https"),
+  server = app.listen(process.env.PORT || 12345, () => {
+  /*   setTimeout (()=>{
+     	console.log(8)
+     fetch("http:/\/localhost:12345/XRequest?id=0&src=http:/\/localhost:8158/test.js")
+   .then(e=>e.text())
+  .then(console.log)
+  },2000);
+  */
+    var port = server.address().port;
+    console.log(`http://localhost:${port}\n-----------`);
+  }),
+  _end="XRequest.res[$][1].remove();XRequest.resId--;"
+
+app.use((req, res, next) => {
+	try{
+	var md="."+req.path.toUpperCase().replace(/\\$|\/$|$/,"/index.js")
+	require(md)(req,res,next)
+  }catch(e){
+  	next()
+  	}
+});
+
+app.use((req, res) => {
+  res.json({
+    status:0
+  })
+});

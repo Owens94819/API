575e083bae33d42659d200fe6eb03ccf78927318
owens <65927654+Owens94819@users.noreply.github.com>
2023-10-22 22:28:05 +0100

    drive api imple..

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const express = require("express"),
     console.log(`http://localhost:${port}\n-----------`);
     // return;
     // setTimeout(() => {
-      let url = "https://www.google.com/search?q=send+stream+as+response+content-type+nodejs&sca_esv=575386901&hl=en&tbm=isch&sxsrf=AM9HkKk_qc6SIlKvRVhCIEAhSdTnngyPbw:1697882354761&source=lnms&sa=X&ved=2ahUKEwjlzoDV8IaCAxXzLUQIHWUhDegQ_AUoAXoECAIQAw&biw=1366&bih=629&dpr=1"
+      // let url = "https://www.google.com/search?q=send+stream+as+response+content-type+nodejs&sca_esv=575386901&hl=en&tbm=isch&sxsrf=AM9HkKk_qc6SIlKvRVhCIEAhSdTnngyPbw:1697882354761&source=lnms&sa=X&ved=2ahUKEwjlzoDV8IaCAxXzLUQIHWUhDegQ_AUoAXoECAIQAw&biw=1366&bih=629&dpr=1"
      // url = "http://localhost:5000/titles/htmx in 100 seconds(720P_HD).mp4"
-      url=encodeURIComponent(url);
-      fetch("http:/\/localhost:1000/my-google-drive?url="+url)
-        .then(e => "done!")
-        .then(console.log) 
+      // url=encodeURIComponent(url);
+      // fetch("http:/\/localhost:1000/my-google-drive?url="+url)
+      //   .then(e => "done!")
+      //   .then(console.log) 
     // }, 1000);
   });
 
@@ -42,4 +42,5 @@ app.use((req, res) => {
 });
 
 //https://x.cyclic.app
+//https://sites.google.com/view/my-g-drive
 global.express = express;

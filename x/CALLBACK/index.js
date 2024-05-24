8cbe76319208ef9f2e04a14155e7a3841a38e25b
owens <65927654+Owens94819@users.noreply.github.com>
2024-05-24 18:26:03 +0100

    query

--- x/CALLBACK/index.js
+++ x/CALLBACK/index.js
@@ -1,5 +1,5 @@
 module.exports=({url,query}, res) => {
-        res.send(717431987)
+        res.send("717431987")
         console.log(query);
     //  res.redirect('http://localhost:3000'+url);
   
